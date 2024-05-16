544d5ddde25f544fffafb9de44c22f27d0caf89d
joseph <joseph.f.tossy91@hotmail.com>
2024-05-16 20:27:36 +0300

    feat: created the mappings and fixed issues wtih offer ML model

--- ml_models/offer_ml_model/offerModel.js
+++ ml_models/offer_ml_model/offerModel.js
@@ -1,6 +1,8 @@
 const tf = require('@tensorflow/tfjs');
 const fs = require('fs');
 const csv = require('csv-parser');
+// get mappings for transforming labels to numberical values
+const { experienceLevelMap, employmentTypeMap, companyLocationMap, companySizeMap, jobTitleMap } = require('./mappings.js');
 
 // implementing the 80% train - 20% test rule 
 async function loadData(filePath, splitRatio = 0.8) {
@@ -29,41 +31,15 @@ async function loadData(filePath, splitRatio = 0.8) {
 }
 
 function preprocessRow(row) {
-    // changing types to numerical values 
-    const experienceLevelMap = { 'SE': 0, 'MI': 1, 'EN': 2, 'EX': 3 };
-    const employmentTypeMap = { 'FT': 0, 'CT': 1 };
-    const companyLocationMap = { 'US': 0, 'CA': 1, 'DE': 2, 'GB': 3, 'IN': 4, 'HK': 5, 'NG': 6, 'ES': 7, 'PT': 8 };
-    const companySizeMap = { 'S': 0, 'M': 1, 'L': 2 };
-    const jobTitleMap = {
-        'Principal Data Scientist': 0,
-        'ML Engineer': 1,
-        'Data Scientist': 2,
-        'Applied Scientist': 3,
-        'Data Analyst': 4,
-        'Data Modeler': 5,
-        'Research Engineer': 6,
-        'Analytics Engineer': 7,
-        'Business Intelligence Engineer': 8,
-        'Machine Learning Engineer': 9,
-        'Data Strategist': 10,
-        'Data Engineer': 11,
-        'Computer Vision Engineer': 12,
-        'Data Architect': 13,
-        'Applied Machine Learning Engineer': 14,
-        'AI Developer': 15,
-        'Data Quality Analyst': 16,
-        'Compliance Data Analyst': 17
-    };
-
     return [
         parseInt(row.work_year),
-        experienceLevelMap[row.experience_level] || -1,
-        employmentTypeMap[row.employment_type] || -1,
-        jobTitleMap[row.job_title] || -1,
+        experienceLevelMap[row.experience_level],
+        employmentTypeMap[row.employment_type],
+        jobTitleMap[row.job_title],
         parseFloat(row.salary_in_usd),
         parseFloat(row.remote_ratio),
-        companyLocationMap[row.company_location] || -1,
-        companySizeMap[row.company_size] || -1
+        companyLocationMap[row.company_location],
+        companySizeMap[row.company_size]
     ];
 }
 
@@ -97,6 +73,22 @@ async function evaluateModel(model, testData) {
     for (let i = 0; i < trueValues.length; i++) {
         console.log(`${trueValues[i][0]}\t\t${predValues[i][0]}`);
     }
+
+    checkErrorMargin(trueValues, predValues);
+    return model;
+}
+
+function checkErrorMargin(trueValues, predValues) {
+    const marginErrors = [];
+    for (let i = 0; i < trueValues.length; i++) {
+        const marginError = Math.abs(trueValues[i][0] - predValues[i][0]);
+        marginErrors.push(marginError);
+    }
+    const meanAbsoluteError = marginErrors.reduce((acc, val) => acc + val, 0) / marginErrors.length;
+    const meanSquaredError = marginErrors.reduce((acc, val) => acc + Math.pow(val, 2), 0) / marginErrors.length;
+
+    console.log(`mean ABSOLUTE error: ${meanAbsoluteError}`);
+    console.log(`mean SQUARED error: ${meanSquaredError}`);
 }
 
 
@@ -113,16 +105,16 @@ async function trainModel(trainingData) {
             onEpochEnd: (epoch, logs) => console.log(`epoch ${epoch}: loss = ${logs.loss}`),
         }
     });
-
     console.log('training completed');
-
     return model;
 }
 
-async function testModel() {
+async function testOfferMlModel() {
     const { trainingData, testingData } = await loadData('./training_data/ds_salaries.csv', 0.8);
     const model = await trainModel(trainingData);
     await evaluateModel(model, testingData);
 }
 
-testModel().catch(console.error);
+
+testOfferMlModel()
+    .catch(console.error);
